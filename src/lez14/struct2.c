70dc98def3bd047cf661b3bc2f9035dac519d1e7
Luca <luca.cardinali@gmail.com>
2026-03-31 05:40:41 +0200

    fine lezione 14

--- src/lez14/struct2.c
+++ src/lez14/struct2.c
@@ -5,7 +5,6 @@
  *      Author: navigazione
  */
 
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
@@ -18,62 +17,46 @@
  * |num | dem |
  * +----+-----+
  * */
-struct fract{
+struct fract {
 	int num;
 	int den;
 };
 
-
-static void set_fraction(int *f, int num, int den) {
-#if 0
-	f[0] = num;
-	f[1] = den;
-#endif
-	*f = num;
-	f = f + 1;
-	*f = den;
-}
-
 /*Create a new fraction, setting num and den as the numerator
  * and denominator of the fraction.
- * The function returns NULL on out of memory, otherwise the
- * fraction object is returned. */
-static int* create_fraction(int num, int den) {
-	int *f = malloc(sizeof(int) * 2);
-	if (f == NULL)
-		return NULL; // malloc error checking.
-	f[0] = num;
-	f[1] = den;
+ */
+static struct fract create_fraction(int num, int den) {
+	struct fract f;
+	f.num = num;
+	f.den = den;
 	return f;
 }
 
 /* Simplify the provided fraction.*/
-static void simplify_fraction(int *f) {
-	for (int d = 2; d <= f[0] && d <= f[1]; d++) {
-		if (f[0] % d == 0 && f[1] % d == 0) {
-			f[0] /= d;
-			f[1] /= d;
+static void simplify_fraction(struct fract *f) {
+	for (int d = 2; d <= f->num && d <= f->den; d++) {
+		if (f->num % d == 0 && f->den % d == 0) {
+			f->num /= d;
+			f->den /= d;
 		}
 	}
 }
 
-static void print_fraction(int *f) {
-	printf("%d/%d\n", f[0], f[1]);
+static void print_fraction(struct fract *f) {
+	printf("%d/%d\n", f->num, f->den);
 }
 
-
-static void fraction_completo_senza_struct() {
-	int *f1 = create_fraction(10, 20);
-	int *f2 = create_fraction(3, 4);
-	simplify_fraction(f1);
-	print_fraction(f1);
-	simplify_fraction(f2);
-	print_fraction(f2);
+static void fraction_completo_con_struct() {
+	struct fract f1 = create_fraction(10, 20);
+	struct fract f2 = create_fraction(3, 4);
+	simplify_fraction(&f1);
+	print_fraction(&f1);
+	simplify_fraction(&f2);
+	print_fraction(&f2);
 }
-
 
 int struct2_main() {
-	fraction_completo_senza_struct();
+	fraction_completo_con_struct();
 
 	return 0;
 }

e8a6a673bd84b68ef5b02c2db1bc5a2c7899d1dc
Luca <luca.cardinali@gmail.com>
2026-04-01 17:52:22 +0200

    pulizia

--- src/lez15/tac.c
+++ src/lez15/tac.c
@@ -20,12 +20,10 @@ struct line {
 #define NOME_FILE "src/lez15/tac.c"
 
 int tac_main(int argc, char **argv) {
-	printf("lezione 15\n");
 	if (argc != 2) {
 		printf("Missing file name\n");
 		return 1;
 	}
-	printf("%s\n", argv[0]);
 	FILE *fp = fopen(argv[1], "r");
 	if (fp == NULL) {
 		printf("File does not exist\n");
@@ -39,7 +37,6 @@ int tac_main(int argc, char **argv) {
 		size_t linelen = strlen(buf);
 		l->s = malloc(linelen + 1);
 		for (size_t j = 0; j <= linelen; j++) {
-			printf("%c", buf[j]);
 			l->s[j] = buf[j];
 		}
 		l->next = head;

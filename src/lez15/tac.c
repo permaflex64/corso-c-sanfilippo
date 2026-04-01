f3ee8c1651e99e030aa80e5c5d8a5957ec40e39f
Luca <luca.cardinali@gmail.com>
2026-04-01 17:48:48 +0200

    affinamenti: include vari

--- src/lez15/tac.c
+++ src/lez15/tac.c
@@ -1,11 +1,15 @@
 /*
  * tac.c
  *
+ *(vedi https://www.geeksforgeeks.org/c/linked-list-in-c/)
  *  Created on: Mar 31, 2026
  *      Author: navigazione
  */
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
 #include "esercizi.h"
 
 struct line {
@@ -14,16 +18,6 @@ struct line {
 };
 
 #define NOME_FILE "src/lez15/tac.c"
-
-#if 0
-// argc è il numero di argomenti (il nome del programma è il primo, quindi argc >= 1)
-// argv è un array di stringhe (puntatori a char)
-static void gemini_configurazione_main(int argc, char **argv) {
-	for (int i = 0; i < argc; ++i) {
-		printf("%d:%s\n", i, argv[i]);
-	}
-}
-#endif
 
 int tac_main(int argc, char **argv) {
 	printf("lezione 15\n");
@@ -41,12 +35,10 @@ int tac_main(int argc, char **argv) {
 	char buf[1024];
 	struct line *head = NULL;
 	while (fgets(buf, sizeof(buf), fp) != NULL) {
-//		printf("%s\n",buf);
 		struct line *l = malloc(sizeof(struct line));
 		size_t linelen = strlen(buf);
-//		printf("%d\n",linelen);
-		l->s = malloc(linelen+1);
-		for (int j = 0; j <= linelen; j++) {
+		l->s = malloc(linelen + 1);
+		for (size_t j = 0; j <= linelen; j++) {
 			printf("%c", buf[j]);
 			l->s[j] = buf[j];
 		}
@@ -55,7 +47,7 @@ int tac_main(int argc, char **argv) {
 	}
 	fclose(fp);
 
-	while(head != NULL){
+	while (head != NULL) {
 		printf("%s", head->s);
 		head = head->next;
 	}
